6972a60869161776c197dab098c3da0bba5a583e
Ranjith Sekar <ranjith.sekar@clarivate.com>
2019-11-12 13:19:08 +0530

    initial commit

---
+++ boot-rest-mongo/src/main/java/com/bootrest/BootRestMongoApplication.java
@@ -0,0 +1,13 @@
+package com.bootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestMongoApplication.class, args);
+	}
+
+}
