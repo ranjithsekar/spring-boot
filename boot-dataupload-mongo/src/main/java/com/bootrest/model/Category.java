af412776639fd173e991a62a3582fd2ee20a5d1d
Ranjith Sekar <ranjith.sekar@clarivate.com>
2019-11-13 11:27:13 +0530

    lombak added

--- boot-dataupload-mongo/src/main/java/com/bootrest/model/Category.java
+++ boot-dataupload-mongo/src/main/java/com/bootrest/model/Category.java
@@ -4,59 +4,20 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 @Document(collection = "category")
 public class Category {
-
   @Id
   private int id;
-
   @Indexed(unique = true)
   private String name;
-
   private String title;
-
   private String description;
-
   private boolean active;
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public boolean isActive() {
-    return active;
-  }
-
-  public void setActive(boolean active) {
-    this.active = active;
-  }
-
 }
--- boot-dataupload-mongo/src/main/java/com/bootrest/model/SubCategory.java
+++ boot-dataupload-mongo/src/main/java/com/bootrest/model/SubCategory.java
@@ -1,53 +1,21 @@
 package com.bootrest.model;
 
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 @Document(collection = "subcategory")
 public class SubCategory {
   private int categoryId;
+  @Indexed(unique = true)
   private String name;
   private String title;
   private String desc;
   private boolean active;
-
-  public int getCategoryId() {
-    return categoryId;
-  }
-
-  public void setCategoryId(int categoryId) {
-    this.categoryId = categoryId;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDesc() {
-    return desc;
-  }
-
-  public void setDesc(String desc) {
-    this.desc = desc;
-  }
-
-  public boolean isActive() {
-    return active;
-  }
-
-  public void setActive(boolean active) {
-    this.active = active;
-  }
-
 }
