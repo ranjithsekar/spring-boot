91831936dd9b78078d6d9e114039f1eccf747510
Ranjith Sekar <ranjith.sekar@clarivate.com>
2020-02-05 20:32:20 +0530

    update

--- sb-ms-user-wishlist/src/main/java/rj/sb/ms/WishlistController.java
+++ sb-ms-user-wishlist/src/main/java/rj/sb/ms/WishlistController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,48 +29,61 @@ public class WishlistController {
   private WebClient.Builder webClient;
 
   @RequestMapping("/{userid}")
-  public List<Wishlist> getUserWishlist(@PathVariable("userid") String userid) {
+  public List<Wishlist> getUserWishlistUsingRestTemplate(@PathVariable("userid") String userid) {
+
+    List<Product> products = way2(userid);
+    return products.stream()
+        .map(prod ->
+          {
+            Product product = restTemplate.getForObject("http://localhost:6060/product/info/" + prod.getId(),
+                Product.class);
+            Rating rating = restTemplate.getForObject("http://localhost:7070/product/rating/" + prod.getId(),
+                Rating.class);
+
+            return new Wishlist(prod.getId(), product.getName(), rating.getRating());
+          })
+        .collect(Collectors.toList());
+  }
+
+  private List<Product> way2(String userid) {
+    ResponseEntity<List<Product>> response = restTemplate.exchange("http://localhost:6060/product/" + userid,
+        HttpMethod.GET, null, new ParameterizedTypeReference<List<Product>>() {
+        });
+    return response.getBody();
+  }
+
+  private List<Product> way1(String userid) {
+    ResponseEntity<Product[]> response = restTemplate.getForEntity("http://localhost:6060/product/" + userid,
+        Product[].class);
+    return Arrays.asList(response.getBody());
+  }
+
+  // @RequestMapping("/{userid}")
+  public List<Wishlist> getUserWishlistUsingWebClient(@PathVariable("userid") String userid) {
     List<Wishlist> wishlists = Arrays.asList(new Wishlist("100", "", ""), new Wishlist("200", "", ""),
         new Wishlist("300", "", ""));
+
     return wishlists.stream()
-        .map(wishlist ->
+        .map(prod ->
           {
             Product product = webClient.build()
                 .get()
-                .uri("http://localhost:6060/product/info/" + wishlist.getProductid())
+                .uri("http://localhost:6060/product/info/" + prod.getProductid())
                 .retrieve()
                 .bodyToMono(Product.class)
                 .block();
 
             Rating rating = webClient.build()
                 .get()
-                .uri("http://localhost:7070/product/rating/" + wishlist.getProductid())
+                .uri("http://localhost:7070/product/rating/" + prod.getProductid())
                 .retrieve()
                 .bodyToMono(Rating.class)
                 .block();
 
-            return new Wishlist(wishlist.getProductid(), product.getName(), rating.getRating());
+            return new Wishlist(prod.getProductid(), product.getName(), rating.getRating());
           })
         .collect(Collectors.toList());
 
   }
 
-  // @RequestMapping("/{userid}")
-  public List<Wishlist> getUserWishlistUsingRestTemplate(@PathVariable("userid") String userid) {
-    List<Wishlist> wishlists = Arrays.asList(new Wishlist("100", "", ""), new Wishlist("200", "", ""),
-        new Wishlist("300", "", ""));
-
-    return wishlists.stream()
-        .map(wishlist ->
-          {
-            Product product = restTemplate.getForObject("http://localhost:6060/product/info/" + wishlist.getProductid(),
-                Product.class);
-            Rating rating = restTemplate.getForObject("http://localhost:7070/product/rating/" + wishlist.getProductid(),
-                Rating.class);
-
-            return new Wishlist(wishlist.getProductid(), product.getName(), rating.getRating());
-          })
-        .collect(Collectors.toList());
-
-  }
 }
