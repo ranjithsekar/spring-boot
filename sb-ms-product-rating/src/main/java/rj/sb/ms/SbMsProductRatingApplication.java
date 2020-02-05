8d1943c9eca2f7c1d79d77741d8a6fe486950a75
Ranjith Sekar <ranjith.sekar@clarivate.com>
2020-02-05 14:44:20 +0530

    update

---
+++ sb-ms-product-rating/src/main/java/rj/sb/ms/SbMsProductRatingApplication.java
@@ -0,0 +1,13 @@
+package rj.sb.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMsProductRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMsProductRatingApplication.class, args);
+	}
+
+}
---
+++ sb-ms-product-rating/src/test/java/rj/sb/ms/SbMsProductRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package rj.sb.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMsProductRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
