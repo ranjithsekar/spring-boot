56a8ea40852e0b9faa10ee2890dda6f61998d157
Ranjith Sekar <ranjith.sekar@clarivate.com>
2020-02-18 18:57:04 +0530

    sample springboot webapp with jsp

---
+++ sb-webapp/src/main/java/jbr/sboot/SbWebappApplication.java
@@ -0,0 +1,13 @@
+package jbr.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbWebappApplication.class, args);
+  }
+
+}
