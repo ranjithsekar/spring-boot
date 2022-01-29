f96b8f729f6c3f2a11523f591dbbbc74faa02f8b
Sekar, Ranjith <ranjith.sekar@Clarivate.com>
2022-01-29 14:12:53 +0530

    test

--- webshop-ms/src/main/java/jbr/webshop/WebshopMsApplication.java
+++ webshop-ms/src/main/java/jbr/webshop/WebshopMsApplication.java
@@ -6,8 +6,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebshopMsApplication {
 
+    @Value("${cors.domain}")
+    private String corsDomain;
+
+    @Value("${cors.headers}")
+    private String corsHeaders;
+
+    @Value("${cors.methods}")
+    private String corsMethods;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebshopMsApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        log.info("corsDomain: {}", corsDomain);
+        log.info("corsHeaders: {}", corsHeaders);
+        log.info("corsMethods: {}", corsMethods);
+        log.info("basePath: {}", basePath);
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(basePath + "/**").allowedOrigins(corsDomain.split(","))
+                        .allowedHeaders(corsHeaders.split(",")).allowedMethods(corsMethods.split(",")).maxAge(3600);
+            }
+        };
+    }
+
 
 }
