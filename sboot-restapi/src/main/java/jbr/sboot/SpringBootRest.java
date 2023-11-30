e588d0154fef8387b58e8927512c7cd231b968ae
Ranjith Sekar <ranjith.sekar@Clarivate.com>
2023-11-30 20:23:23 +0530

    Get db details

--- sboot-restapi/src/main/java/jbr/sboot/SpringBootRest.java
+++ sboot-restapi/src/main/java/jbr/sboot/SpringBootRest.java
@@ -1,5 +1,11 @@
 package jbr.sboot;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,12 +31,16 @@ public class SpringBootRest {
   @Value("${spring.data.rest.base-path}")
   private String basePath;
 
+  @Autowired
+  private DataSource dataSource;
+
   public static void main(String[] args) {
     SpringApplication.run(SpringBootRest.class, args);
   }
 
   @Bean
-  public WebMvcConfigurer corsConfigurer() {
+  public WebMvcConfigurer corsConfigurer() throws Exception{
+    this.showTables();
     log.info("corsDomain: {}", corsDomain);
     log.info("corsHeaders: {}", corsHeaders);
     log.info("corsMethods: {}", corsMethods);
@@ -39,13 +49,28 @@ public class SpringBootRest {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping(basePath + "/**")
-            .allowedOrigins(corsDomain.split(","))
-            .allowedHeaders(corsHeaders.split(","))
-            .allowedMethods(corsMethods.split(","))
-            .maxAge(3600);
+        registry.addMapping(basePath + "/**").allowedOrigins(corsDomain.split(","))
+            .allowedHeaders(corsHeaders.split(",")).allowedMethods(corsMethods.split(",")).maxAge(3600);
       }
     };
   }
 
+  public void showTables() throws Exception {
+    DatabaseMetaData metaData = dataSource.getConnection().getMetaData();
+    System.out.println("Schema: " + dataSource.getConnection().getSchema());
+    System.out.println("Url: " + metaData.getURL());
+    System.out.println("Username: " + metaData.getUserName());
+    ResultSet tables = metaData.getTables(null, null, null, new String[] { "TABLE" });
+
+    while (tables.next()) {
+      String tableName = tables.getString("TABLE_NAME");
+      System.out.println(tableName);
+      ResultSet columns = metaData.getColumns(null, null, tableName, null);
+
+      while (columns.next()) {
+        String columnName = columns.getString("COLUMN_NAME");
+        System.out.println("\t" + columnName);
+      }
+    }
+  }
 }
